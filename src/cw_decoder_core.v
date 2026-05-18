b7595a0c54fc8be03f0b52124d413f2cf0b50778
KerCrafter <clemkerneur@gmail.com>
2026-05-18 15:05:43 +0200

    manage I & M char

--- src/cw_decoder_core.v
+++ src/cw_decoder_core.v
@@ -8,21 +8,41 @@ module cw_decoder_core (
 );
 
   reg [2:0] sig_len;
+  reg sig_end;
+
+  reg [2:0] sig2_len;
 
   always @(posedge clk) begin
     if(reset) begin
       sig_len <= 0;
+      sig_end <= 0;
+
+      sig2_len <= 0;
       serial_line <= 0;
 
     end else begin
       if(cw_sig) begin
-        sig_len = sig_len + 1;
+        if(sig_end == 0) begin
+          sig_len = sig_len + 1;
+        end
+
+        if(sig_end == 1) begin
+          sig2_len = sig2_len + 1;
+        end
+      end else begin
+        if(sig_len > 0) begin
+          sig_end <= 1;
+        end
       end
 
-      if(sig_len == 1) begin
+      if(sig_len == 1 && sig2_len == 0) begin
         serial_line <= 69;
-      end else begin
+      end else if(sig_len > 1 && sig2_len == 0) begin
         serial_line <= 84;
+      end else if(sig2_len == 1) begin
+        serial_line <= 73;
+      end else begin
+        serial_line <= 77;
       end
     end
   end
--- tests/test.py
+++ tests/test.py
@@ -31,6 +31,11 @@ async def simulate_DIT(dut):
   dut.cw_sig.value = '0'
   await Timer(1, unit='ns');
 
+  dut.clk.value = '1';
+  await Timer(1, unit='ns');
+  dut.clk.value = '0';
+  await Timer(1, unit='ns');
+
 async def simulate_DAH(dut):
   dut.cw_sig.value = '1'
   await Timer(1, unit='ns');
@@ -46,6 +51,11 @@ async def simulate_DAH(dut):
   await Timer(1, unit='ns');
 
   dut.cw_sig.value = '0'
+  await Timer(1, unit='ns');
+
+  dut.clk.value = '1';
+  await Timer(1, unit='ns');
+  dut.clk.value = '0';
   await Timer(1, unit='ns');
 
 async def simulate_CHAR_SPACE(dut):
@@ -78,3 +88,26 @@ async def decode_T_char(dut):
   await simulate_CHAR_SPACE(dut);
 
   assert dut.serial_line.value == 84
+
+
+@cocotb.test()
+async def decode_I_char(dut):
+  await inital_reset(dut);
+
+  await simulate_DIT(dut);
+  await simulate_DIT(dut);
+
+  await simulate_CHAR_SPACE(dut);
+
+  assert dut.serial_line.value == 73 
+
+@cocotb.test()
+async def decode_M_char(dut):
+  await inital_reset(dut);
+
+  await simulate_DAH(dut);
+  await simulate_DAH(dut);
+
+  await simulate_CHAR_SPACE(dut);
+
+  assert dut.serial_line.value == 77 
